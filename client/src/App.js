6154a427e731bc43e8a4a033240908914853f9e4
Eva Lieu <itsevalieu@gmail.com>
2018-12-22 22:14:05 -0800

    Added MuiThemeProvider, wrapped around root component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { createMuiTheme } from '@material-ui/core/styles';
+import green from '@material-ui/core/colors/green';
 import Header from './containers/App/components/Header/Header.jsx';
+
+const theme = createMuiTheme ({
+  palette: {
+    primary: green
+  }
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header/>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Header/>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
