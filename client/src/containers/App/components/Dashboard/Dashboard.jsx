cde0a67d86d07c1883e1c58848399630b0cd1c48
Eva Lieu <itsevalieu@gmail.com>
2019-01-11 20:37:26 -0800

    Adding Grid, styles

--- client/src/containers/App/components/Dashboard/Dashboard.jsx
+++ client/src/containers/App/components/Dashboard/Dashboard.jsx
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 import './Dashboard.css';
+import Grid from '@material-ui/core/Grid';
+import Block from './components/Block';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
 
-class Dashboard extends Component {
-  render() {
-    return (
-        <div id='dashboard' className='dashboard'>
-            Dashboard
-        </div>
-    );
-  }
+const styles = theme => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+  },
+  toolbar: theme.mixins.toolbar,
+});
+
+function Dashboard(props) {
+  const { classes } = props;
+  return (
+      <main position={'hidden'} className={classes.content}>
+      <div className={classes.toolbar} />
+          <Grid spacing={16}>
+            <Block/><Block/><Block/>
+          </Grid>
+      </main>
+  );
 }
 
-export default Dashboard;
+Dashboard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Dashboard);
