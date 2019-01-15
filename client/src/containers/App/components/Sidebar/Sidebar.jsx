ab8e9f34bf159f95776c5d2c217ba3f5a8737890
Eva Lieu <itsevalieu@gmail.com>
2019-01-14 20:11:14 -0800

    Fix sidebar under header

--- client/src/containers/App/components/Sidebar/Sidebar.jsx
+++ client/src/containers/App/components/Sidebar/Sidebar.jsx
@@ -15,22 +15,25 @@ import MailIcon from '@material-ui/icons/Mail';
 const drawerWidth = 200;
 
 const styles = theme => ({
-  root: {
-    display: 'flex',
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing.unit * 3,
-  },
-
-});
+    root: {
+      display: 'flex',
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing.unit * 3,
+    },
+    toolbar: theme.mixins.toolbar,
+  });
 
 function Sidebar(props) {
   const { classes } = props;
