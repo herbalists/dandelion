6ff5d78e1244bde432d1e5435041d9e58d6da51b
Eva Lieu <itsevalieu@gmail.com>
2019-01-15 18:17:11 -0800

    Removing Sidebar component from App container

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import { createMuiTheme } from '@material-ui/core/styles';
-import './App.css';
-import Header from './components/Header/Header.jsx';
-import Sidebar from './components/Sidebar/Sidebar.jsx';
-import Dashboard from './components/Dashboard/Dashboard.jsx';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
+import Dashboard from './components/Dashboard/Dashboard.jsx';
+import Header from './components/Header/Header.jsx';
+import './App.css';
 
 const theme = createMuiTheme ({
   palette: {
@@ -19,12 +19,9 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
-        {/* <Header/>
-        <Sidebar/>
-        <Dashboard/> */}
+        <CssBaseline />
         <Grid container spacing={0}>
           <Grid item xs={12}><Header/></Grid>
-          <Grid item xs={3}><Sidebar/></Grid>
           <Grid item xs={9}><Dashboard/></Grid>
         </Grid>
       </MuiThemeProvider>
