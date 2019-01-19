d0307af26b15c1bac952884a41b0d29834bd3ef7
Eva Lieu <itsevalieu@gmail.com>
2019-01-18 23:37:45 -0800

    Add appBarShift class on menu open

--- client/src/containers/App/components/Header/Header.jsx
+++ client/src/containers/App/components/Header/Header.jsx
@@ -1,109 +1,90 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Switch from '@material-ui/core/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormGroup from '@material-ui/core/FormGroup';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import './Header.css';
+
 const drawerWidth = 200;
 const styles = theme => ({
   root: {
     flexGrow: 1,
   },
+  toolbar: {
+    paddingRight: 24, // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
   grow: {
     flexGrow: 1,
   },
   menuButton: {
-    marginLeft: -12,
+    marginLeft: 12,
     marginRight: 20,
   },
- 
+  menuButtonHidden: {
+    display: 'none',
+  },
 });
 
 class MenuAppBar extends React.Component {
   state = {
-    auth: true,
-    anchorEl: null,
+    open: false,
   };
 
-  handleChange = event => {
-    this.setState({ auth: event.target.checked });
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
   };
 
-  handleMenu = event => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
-
-  handleClose = () => {
-    this.setState({ anchorEl: null });
+  handleDrawerClose = () => {
+    this.setState({ open: false });
   };
 
   render() {
     const { classes } = this.props;
-    const { auth, anchorEl } = this.state;
-    const open = Boolean(anchorEl);
+    const { open } = this.state;
 
     return (
       <div className='header'>
         <div className={classes.root}>
-          {/* <FormGroup>
-            <FormControlLabel
-              control={
-                <Switch checked={auth} onChange={this.handleChange} aria-label="LoginSwitch" />
-              }
-              label={auth ? 'Logout' : 'Login'}
-            />
-          </FormGroup> */}
-          <CssBaseline />
-          <AppBar position="fixed" className={classes.appBar}>
-            <Toolbar>
-              <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+          <AppBar position="fixed" className={classNames(classes.appBar, {[classes.appBarShift]: open,})}>
+            <Toolbar disableGutters={!this.state.open} className={classes.toolbar}>
+              <IconButton 
+                color="inherit"
+                aria-label="Open drawer"
+                onClick={this.handleDrawerOpen}
+                className={classNames(
+                  classes.menuButton,
+                  open && classes.menuButtonHidden,
+                )}>
                 <MenuIcon />
               </IconButton>
               <Typography variant="h6" color="inherit" className={classes.grow}>
                 Dandelion - Your Personal Dashboard
               </Typography>
-              {auth && (
-                <div>
-                  <IconButton
-                    aria-owns={open ? 'menu-appbar' : undefined}
-                    aria-haspopup="true"
-                    onClick={this.handleMenu}
-                    color="inherit"
-                  >
-                    <AccountCircle />
-                  </IconButton>
-                  <Menu
-                    id="menu-appbar"
-                    anchorEl={anchorEl}
-                    anchorOrigin={{
-                      vertical: 'top',
-                      horizontal: 'right',
-                    }}
-                    transformOrigin={{
-                      vertical: 'top',
-                      horizontal: 'right',
-                    }}
-                    open={open}
-                    onClose={this.handleClose}
-                  >
-                    <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                    <MenuItem onClick={this.handleClose}>My account</MenuItem>
-                  </Menu>
-                </div>
-              )}
             </Toolbar>
           </AppBar>
         </div>
